d79cc04c5877f9181b3b82cb7a4d4d04b4ddcc8c
ShaanSharma3 <148427222+ShaanSharma3@users.noreply.github.com>
2024-05-22 00:07:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const express = require("express");  //to import module
-const mongoose = require("mongoose");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
 
 
-const app = express();  //to construct an app using express
-const pinRoute = require("./roots/pins");
-const userRoute = require("./roots/users");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.use(express.json());
-
-mongoose.connect   //for establishing connection
-('mongodb+srv://shaan:shaan27@cluster0.usiea9i.mongodb.net/shaan?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => {
-    console.log("Mongo DB connected")
-}).catch(err => console.log(err));
-
-
-app.use("/api/users",userRoute);
-app.use("/api/pins",pinRoute);
-
-
-app.listen(8800,() =>{
-    console.log("backend server is running!")
-})
